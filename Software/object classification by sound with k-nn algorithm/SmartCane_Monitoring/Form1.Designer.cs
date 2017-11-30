2049acb227a84701eea8ead4dbb38d16a10018c5
Wonse Jo <jow@purdue.edu>
2017-11-30 01:23:26 -0500

    update k-NN monitoring program

---
+++ Software/object classification by sound with k-nn algorithm/SmartCane_Monitoring/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace SmartCane_Monitoring
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.serialPort_conboBox = new System.Windows.Forms.ComboBox();
+            this.Serial_connect = new System.Windows.Forms.Button();
+            this.Serial_Stop = new System.Windows.Forms.Button();
+            this.Serial_Port_COM = new System.IO.Ports.SerialPort(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label1);
+            this.groupBox4.Controls.Add(this.comboBox1);
+            this.groupBox4.Controls.Add(this.serialPort_conboBox);
+            this.groupBox4.Controls.Add(this.Serial_connect);
+            this.groupBox4.Controls.Add(this.Serial_Stop);
+            this.groupBox4.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.groupBox4.Location = new System.Drawing.Point(855, 13);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox4.Size = new System.Drawing.Size(224, 203);
+            this.groupBox4.TabIndex = 29;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Serial Port";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 38;
+            this.label1.Text = "K-Value";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "3",
+            "5",
+            "7",
+            "9",
+            "11"});
+            this.comboBox1.Location = new System.Drawing.Point(9, 148);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(91, 24);
+            this.comboBox1.TabIndex = 37;
+            this.comboBox1.Text = "3";
+            // 
+            // serialPort_conboBox
+            // 
+            this.serialPort_conboBox.FormattingEnabled = true;
+            this.serialPort_conboBox.Location = new System.Drawing.Point(9, 25);
+            this.serialPort_conboBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.serialPort_conboBox.Name = "serialPort_conboBox";
+            this.serialPort_conboBox.Size = new System.Drawing.Size(91, 24);
+            this.serialPort_conboBox.TabIndex = 24;
+            this.serialPort_conboBox.DropDown += new System.EventHandler(this.serialPort_conboBox_DropDown);
+            // 
+            // Serial_connect
+            // 
+            this.Serial_connect.Font = new System.Drawing.Font("굴림", 9F);
+            this.Serial_connect.Location = new System.Drawing.Point(9, 69);
+            this.Serial_connect.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Serial_connect.Name = "Serial_connect";
+            this.Serial_connect.Size = new System.Drawing.Size(89, 37);
+            this.Serial_connect.TabIndex = 5;
+            this.Serial_connect.Text = "Connect";
+            this.Serial_connect.UseVisualStyleBackColor = true;
+            this.Serial_connect.Click += new System.EventHandler(this.Serial_connect_Click);
+            // 
+            // Serial_Stop
+            // 
+            this.Serial_Stop.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Serial_Stop.Location = new System.Drawing.Point(112, 69);
+            this.Serial_Stop.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Serial_Stop.Name = "Serial_Stop";
+            this.Serial_Stop.Size = new System.Drawing.Size(89, 35);
+            this.Serial_Stop.TabIndex = 7;
+            this.Serial_Stop.Text = "Disconnect";
+            this.Serial_Stop.UseVisualStyleBackColor = true;
+            this.Serial_Stop.Click += new System.EventHandler(this.Serial_Stop_Click);
+            // 
+            // Serial_Port_COM
+            // 
+            this.Serial_Port_COM.BaudRate = 115200;
+            this.Serial_Port_COM.PortName = "COM8";
+            this.Serial_Port_COM.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.Serial_Port_COM_DataReceived);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Top;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 5;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Legend = "Legend1";
+            series1.MarkerBorderWidth = 5;
+            series1.MarkerSize = 10;
+            series1.Name = "Wood";
+            series2.BorderWidth = 5;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Legend = "Legend1";
+            series2.MarkerBorderWidth = 5;
+            series2.MarkerSize = 10;
+            series2.Name = "Paper";
+            series3.BorderWidth = 5;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series3.Legend = "Legend1";
+            series3.MarkerBorderWidth = 5;
+            series3.MarkerSize = 10;
+            series3.Name = "Plastic";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series4.Legend = "Legend1";
+            series4.MarkerSize = 10;
+            series4.Name = "Metal";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series5.Legend = "Legend1";
+            series5.MarkerBorderColor = System.Drawing.Color.Red;
+            series5.MarkerBorderWidth = 2;
+            series5.MarkerColor = System.Drawing.Color.Yellow;
+            series5.MarkerSize = 20;
+            series5.Name = "New";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bubble;
+            series6.Legend = "Legend1";
+            series6.MarkerBorderColor = System.Drawing.Color.Red;
+            series6.MarkerBorderWidth = 3;
+            series6.MarkerColor = System.Drawing.Color.Transparent;
+            series6.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series6.Name = "KNN";
+            series6.YValuesPerPoint = 2;
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(849, 555);
+            this.chart1.TabIndex = 30;
+            this.chart1.Text = "chart1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(855, 223);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(224, 332);
+            this.richTextBox1.TabIndex = 32;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1091, 565);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.groupBox4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ComboBox serialPort_conboBox;
+        private System.Windows.Forms.Button Serial_connect;
+        private System.Windows.Forms.Button Serial_Stop;
+        private System.IO.Ports.SerialPort Serial_Port_COM;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Software/object classification by sound with k-nn algorithm/SmartCane_Monitoring/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartCane_Monitoring
+{
+    public partial class Form1 : Form
+    {
+        public const int Datalength = 400; //4096
+        public const int R = 50;
+
+        string[] strWONSU0513 = new string[3];
+        string[,] total_data = new string[Datalength, 5];
+        string[] recevier_data = new string[2];
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisX.Maximum = 1000;
+            chart1.ChartAreas[0].AxisY.Minimum = 50;
+            chart1.ChartAreas[0].AxisY.Maximum = 100;
+         
+
+
+            string input_1_path = @"C:\Users\wonsu\Documents\GitHub\MonitoringSensorData\soundMonitoring\SmartCane_Monitoring\SmartCane_Monitoring\wood.txt";
+            string[] input_1_name = System.IO.File.ReadAllLines(input_1_path);
+
+            for (int i = 0; i < input_1_name.Length; i++)
+            {
+                string[] split_data = input_1_name[i].Split(';');
+                string[] second_split_data = split_data[0].Split(',');
+
+                chart1.Series["Wood"].Points.AddXY(int.Parse(second_split_data[0]), float.Parse(second_split_data[1]));
+
+
+                total_data[i, 0] = second_split_data[0];
+                total_data[i, 1] = second_split_data[1];
+                total_data[i, 2] = "wood"; //types
+                total_data[i, 3] = ""; //distance
+                total_data[i, 4] = ""; //rank
+
+
+            }
+
+            string input_2_path = @"C:\Users\wonsu\Documents\GitHub\MonitoringSensorData\soundMonitoring\SmartCane_Monitoring\SmartCane_Monitoring\paper.txt";
+            string[] input_2_name = System.IO.File.ReadAllLines(input_2_path);
+
+            for (int i = 0; i < input_2_name.Length; i++)
+            {
+                string[] split_data = input_2_name[i].Split(';');
+                string[] second_split_data = split_data[0].Split(',');
+
+                chart1.Series["Paper"].Points.AddXY(int.Parse(second_split_data[0]), float.Parse(second_split_data[1]));
+                total_data[i + Datalength / 4, 0] = second_split_data[0];
+                total_data[i + Datalength / 4, 1] = second_split_data[1];
+                total_data[i + Datalength / 4, 2] = "paper"; //types
+                total_data[i + Datalength / 4, 3] = ""; //distance
+                total_data[i + Datalength / 4, 4] = ""; //rank
+
+            }
+
+
+            string input_3_path = @"C:\Users\wonsu\Documents\GitHub\MonitoringSensorData\soundMonitoring\SmartCane_Monitoring\SmartCane_Monitoring\plastic.txt";
+            string[] input_3_name = System.IO.File.ReadAllLines(input_3_path);
+
+            for (int i = 0; i < input_3_name.Length; i++)
+            {
+                string[] split_data = input_3_name[i].Split(';');
+                string[] second_split_data = split_data[0].Split(',');
+
+                chart1.Series["Plastic"].Points.AddXY(int.Parse(second_split_data[0]), float.Parse(second_split_data[1]));
+                total_data[i + Datalength / 2, 0] = second_split_data[0];
+                total_data[i + Datalength / 2, 1] = second_split_data[1];
+                total_data[i + Datalength / 2, 2] = "plastic"; //types
+                total_data[i + Datalength / 2, 3] = ""; //distance
+                total_data[i + Datalength / 2, 4] = ""; //rank
+
+            }
+
+            string input_4_path = @"C:\Users\wonsu\Documents\GitHub\MonitoringSensorData\soundMonitoring\SmartCane_Monitoring\SmartCane_Monitoring\metal.txt";
+
+            string[] input_4_name = System.IO.File.ReadAllLines(input_4_path);
+
+            for (int i = 0; i < input_4_name.Length; i++)
+            {
+                string[] split_data = input_4_name[i].Split(';');
+                string[] second_split_data = split_data[0].Split(',');
+
+                chart1.Series["Metal"].Points.AddXY(int.Parse(second_split_data[0]), float.Parse(second_split_data[1]));
+                total_data[i + Datalength / 4 * 3, 0] = second_split_data[0];
+                total_data[i + Datalength / 4 * 3, 1] = second_split_data[1];
+                total_data[i + Datalength / 4 * 3, 2] = "metal"; //types
+                total_data[i + Datalength / 4 * 3, 3] = ""; //distance
+                total_data[i + Datalength / 4 * 3, 4] = ""; //rank
+
+
+            }
+          
+        }
+
+        private void Serial_Port_COM_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string strReceivedData = Serial_Port_COM.ReadLine();
+            if (strReceivedData.Length == 0)
+                return;
+            else
+            {
+                strWONSU0513 = strReceivedData.Split(',',';');
+                recevier_data[0] = strWONSU0513[0];
+                recevier_data[1] = strWONSU0513[1];
+
+                this.Invoke(new EventHandler(KNN_Method));
+            }
+        }
+
+
+        private void KNN_Method(object s, EventArgs e)
+        {
+            richTextBox1.Clear();
+
+            chart1.Series["New"].Points.Clear();
+            chart1.Series["KNN"].Points.Clear();
+
+            //for TEST 
+          
+            chart1.Series["New"].Points.AddXY(int.Parse(recevier_data[0]), float.Parse(recevier_data[1]));
+            Invoke(new Action(() => richTextBox1.AppendText(recevier_data[0] + "," + recevier_data[1] + "\n")));
+
+            Distance_KNN();
+            SortRank_KNN();
+            FindDataName_KNN();
+        }
+
+
+        private void Distance_KNN()
+        {
+            double distance = 0;
+            double X, Y;
+            
+            for(int i=0;i< Datalength; i++)
+            {
+                if (total_data[i, 0] == null)
+                    return;
+
+                X = Math.Pow( double.Parse(recevier_data[0])- double.Parse(total_data[i, 0]), 2);//(x1-x2)^2
+                Y =Math.Pow(double.Parse(recevier_data[1])-double.Parse(total_data[i, 1]), 2);//(y1-y2)^2
+                distance = Math.Round(Math.Sqrt(X + Y),2);
+                total_data[i, 3] = System.Convert.ToString(distance);            
+            }
+        }
+
+
+        private void SortRank_KNN()
+        {
+            string temp0,temp1,temp2, temp3, temp4 ;
+
+            
+            for (int i = 0; i < Datalength; i++)
+            {
+                for (int j = i + 1; j < Datalength; j++)
+                {
+                    if (double.Parse(total_data[i, 3]) > double.Parse(total_data[j, 3]))
+                    {
+                        temp0 = total_data[i, 0];
+                        temp1 = total_data[i, 1];
+                        temp2 = total_data[i, 2];
+                        temp3 = total_data[i, 3];
+                        temp4 = total_data[i, 4];
+                      
+                        total_data[i, 0] = total_data[j, 0];
+                        total_data[i, 1] = total_data[j, 1];
+                        total_data[i, 2] = total_data[j, 2];
+                        total_data[i, 3] = total_data[j, 3];
+                        total_data[i, 4] = total_data[j, 4];
+
+                        total_data[j, 0] = temp0;
+                        total_data[j, 1] = temp1;
+                        total_data[j, 2] = temp2;
+                        total_data[j, 3] = temp3;
+                        total_data[j, 4] =temp4;
+                        
+                    }
+                }
+            }
+            
+        }
+
+        private void FindDataName_KNN()
+        {           
+            double K_value = double.Parse(comboBox1.Text);
+            double wood_Num = 0;
+            double paper_Num = 0;
+            double plastic_Num = 0;
+            double metal_Num = 0;
+      
+                for (int i = 0; i < K_value; i++)
+                {
+                    if (total_data[i, 2] == "wood")
+                        wood_Num++;
+                    else if (total_data[i, 2] == "paper")
+                        paper_Num++;
+                    else if (total_data[i, 2] == "plastic")
+                        plastic_Num++;
+                    else if (total_data[i, 2] == "metal")
+                        metal_Num++;
+                }
+
+
+                double[] Count_num = { wood_Num, paper_Num, plastic_Num, metal_Num };
+
+                richTextBox1.AppendText("Wood=" + wood_Num);
+                richTextBox1.AppendText(System.Environment.NewLine);
+                richTextBox1.AppendText("Paper=" + paper_Num);
+                richTextBox1.AppendText(System.Environment.NewLine);
+                richTextBox1.AppendText("Plastic=" + plastic_Num);
+                richTextBox1.AppendText(System.Environment.NewLine);
+                richTextBox1.AppendText("Metal=" + metal_Num);
+                richTextBox1.AppendText(System.Environment.NewLine);
+
+
+
+                for (int i = 0; i < K_value; i++)
+                {
+                    chart1.Series["KNN"].Points.AddXY(int.Parse(total_data[i, 0]), float.Parse(total_data[i, 1]));
+                    richTextBox1.AppendText("("+1+")" + total_data[i, 0] + "/" + total_data[i, 1] + "/" + total_data[i, 3]);
+                    richTextBox1.AppendText(System.Environment.NewLine);
+
+                }
+
+                double maxValue = Count_num.Max();
+                double maxIndex = Count_num.ToList().IndexOf(maxValue);
+
+                if (maxIndex == 0)
+                {
+                    richTextBox1.AppendText("Wood");
+                    richTextBox1.AppendText(System.Environment.NewLine);
+                }
+
+                else if (maxIndex == 1)
+                {
+                    richTextBox1.AppendText("Paper");
+                    richTextBox1.AppendText(System.Environment.NewLine);
+
+                }
+
+                else if (maxIndex == 2)
+                {
+                    richTextBox1.AppendText("Plastic");
+                    richTextBox1.AppendText(System.Environment.NewLine);
+
+                }
+
+                else if (maxIndex == 3)
+                {
+                    richTextBox1.AppendText("Metal");
+                    richTextBox1.AppendText(System.Environment.NewLine);
+                }
+
+     
+        }
+
+        private void Serial_connect_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Serial_Port_COM.PortName = serialPort_conboBox.Text;
+                Serial_Port_COM.Open(); //시리얼 열기  
+
+                if (Serial_Port_COM.IsOpen)
+                {
+                    Invoke(new Action(() => richTextBox1.Clear()));
+                    Invoke(new Action(() => MessageBox.Show("Connect to Serial port" + serialPort_conboBox.Text + "\n")));
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Select the number of serial");
+                return;
+            }
+        }
+     
+
+        private void Serial_Stop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Serial_Port_COM.Close();
+                richTextBox1.AppendText("Disconnect" + '\n');
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void serialPort_conboBox_DropDown(object sender, EventArgs e)
+        {
+            serialPort_conboBox.Text = "";
+            serialPort_conboBox.Items.Clear();
+            string[] s;
+            s = System.IO.Ports.SerialPort.GetPortNames();
+            for (int i = 0; i < s.Length; i++)
+            {
+                serialPort_conboBox.Items.Add(s[i]);
+            }
+        }
+    }
+}
